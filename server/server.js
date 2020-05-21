6a1a71c5043878357eaeac2dcd3025c1d962c7f6
Joe Egan <joeegan2202@gmail.com>
2020-05-21 17:17:27 -0400

    Refactored CRA

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const mongo = require('mongodb').MongoClient({useUnifiedTopology: true})
+const crypto = require('crypto')
+const cors = require('cors')
+const app = express()
+app.use(cors())
+const port = 3500
+
+var main = null
+
+mongo.connect('mongodb://127.0.0.1:4000', (err, result) => { // Load database to main
+  if (err) {
+    console.log(`Mongo could not connect: ${err}`)
+    return
+  }
+
+  main = result.db('main')
+  main.collection('sessions').deleteMany({}) // Automatically delete all sessions if server restarts
+  setInterval(() => {
+    main.collection('sessions').find({}).toArray((err, find) => {
+      if (err) return
+
+      for (let session of find) {
+        if (Date.now() - session.time > 30 * 60 * 1000) {
+          main.collection('sessions').deleteOne(session)
+        }
+      }
+    })
+  }, 1000)
+
+  app.listen(port, () => console.log('Started server...'))
+})
+
