632867912e3a80ce664c9337b1ce75e37a839ccc
Joe Egan <joeegan2202@gmail.com>
2020-05-24 15:08:23 -0400

    Changed certificate location

--- server/server.js
+++ server/server.js
@@ -8,9 +8,9 @@ const app = express()
 app.use(cors())
 const port = 3500
 
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8')
-const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8')
-const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8')
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/trivia.eganshub.net/privkey.pem', 'utf8')
+const certificate = fs.readFileSync('/etc/letsencrypt/live/trivia.eganshub.net/cert.pem', 'utf8')
+const ca = fs.readFileSync('/etc/letsencrypt/live/trivia.eganshub.net/chain.pem', 'utf8')
 
 const credentials = {
   key: privateKey,
