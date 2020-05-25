fc78f39d3d1da61ac9cc8ca8a84b1f77465e483f
Joe Egan <joeegan2202@gmail.com>
2020-05-25 12:08:52 -0400

    Imported utils into the utils file

--- server/utils.js
+++ server/utils.js
@@ -1,26 +1,27 @@
 // Generic utilities
+const utils = require('./utils')
 
 module.exports = {
-    verifySessionId,
-    main
+  verifySessionId,
+  main
 }
 
 var main = null
 
 function verifySessionId(session, uname, callback) {
-    main.collection('sessions').findOne({ session, uname }, (err, find) => {
-        if (find) {
-            if (Date.now() - find.time < 30 * 60 * 1000) {
-                let result = { session: find.session, uname: find.uname }
-                console.log(`Session authenticated: ${find.session}`)
+  utils.main.collection('sessions').findOne({ session, uname }, (err, find) => {
+    if (find) {
+      if (Date.now() - find.time < 30 * 60 * 1000) {
+        let result = { session: find.session, uname: find.uname }
+        console.log(`Session authenticated: ${find.session}`)
 
-                callback(result)
-            } else {
-                main.collection('sessions').deleteOne({ session })
-                callback(false)
-            }
-        } else {
-            callback(false)
-        }
-    })
+        callback(result)
+      } else {
+        utils.main.collection('sessions').deleteOne({ session })
+        callback(false)
+      }
+    } else {
+      callback(false)
+    }
+  })
 }
