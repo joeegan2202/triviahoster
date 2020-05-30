9ce10a0b29bf0b5e747b81862917c2894d330dc2
Joe Egan <joeegan2202@gmail.com>
2020-05-29 21:54:08 -0400

    Added basic play and admin components

---
+++ src/Admin.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default class Admin extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div id="admin">
+
+      </div>
+    )
+  }
+}
---
+++ src/Play.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default class Play extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div id="play">
+
+      </div>
+    )
+  }
+}
