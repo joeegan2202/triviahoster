5415c7c47ac345594e5ae3a48d295f369c635295
Joe Egan <joeegan2202@gmail.com>
2020-05-29 21:57:23 -0400

    Changed onclicks to just reference function, not call them. (Rookie mistake)

--- src/Join.js
+++ src/Join.js
@@ -14,9 +14,9 @@ export default class Join extends React.Component {
       <div id="join">
         <div id="header">
           <h1>Play Trivia</h1>
-          <a onClick={this.props.login()}>Host Login</a>
+          <a onClick={this.props.login}>Host Login</a>
         </div>
-        <form onSubmit={this.props.join()}>
+        <form onSubmit={this.props.join}>
           <input id="roomNumber" defaultValue="Room Number:"></input>
           <input id="password" defaultValue="Room Password:"></input>
           <button id="submit" type="submit">Join Room</button>
